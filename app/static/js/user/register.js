89ce3a6781132436e167b6c475284beb13d44cea
YuQibing <1174797939@qq.com>
2017-12-01 17:11:17 +0800

    fix the verify code bug

--- app/static/js/user/register.js
+++ app/static/js/user/register.js
@@ -5,7 +5,7 @@ $(function () {
         $('#freshCode').bind('click', function () {
             var getTimestamp=new Date().getTime();
             console.log('click')
-            $('#verifyCode').attr('src','/user/Verify?time=' + getTimestamp)
+            $('#verifyCode').attr('src','{{url_for("user.verifyCode")' + '?time=' + getTimestamp)
         })
 })
 
